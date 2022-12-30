91657b4b55200aacfe0666ccb6a44980e2bfe7f1
Bernard <l3ernardz@gmail.com>
2022-12-30 17:02:01 +0100

    Folder Layout CMS UI
    HTML and css beginnings

---
+++ cms/index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Beru CMS Admin Panel</title>
+  <meta name="description" content="A simple HTML5 Template for new projects.">
+  <meta name="author" content="Bernard Stornebrink">
+  <link rel="stylesheet" href="../style/default.css?v=1.0">
+
+</head>
+
+<body>
+  
+    <div id="page-wrapper">
+
+        <div id="admin-panel">
+            <div id="admin-panel-menu">
+                <p class="admin-panel-user">Welcome <b>Admin</b></p> <!-- replace a with php get current user -->
+
+                <ul>
+                    <li><a href="#">User Control</a></li>
+                    
+                    <li><a href="#">Page Control</a></li>
+
+                    <li><a href="#">Theme</a></li>
+
+                    
+                    <li><a href="#">Logout</a></li>
+                </ul>
+        
+            </div>
+
+            <div id="admin-panel-content">
+                <!-- current control content -->
+            </div>
+        </div>
+
+    </div>
+  <script src="js/scripts.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Beru CMS</title>
+  <meta name="description" content="A simple HTML5 Template for new projects.">
+  <meta name="author" content="SitePoint">
+
+  <meta property="og:title" content="A Basic HTML5 Template">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="">
+  <meta property="og:description" content="A simple HTML5 Template for new projects.">
+
+
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+</head>
+
+<body>
+  testing testing
+  <script src="js/scripts.js"></script>
+</body>
+</html>
